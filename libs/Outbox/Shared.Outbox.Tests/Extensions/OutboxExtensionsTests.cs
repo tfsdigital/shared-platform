19ad39450da4066884c5dda82e2b684c78626970
Tiago Santos <tfsantosbr@gmail.com>
2026-04-27 09:31:38 -0300

    fix(outbox-tests): use concrete ServiceCollection return type (CA1859)

--- libs/Outbox/Shared.Outbox.Tests/Extensions/OutboxExtensionsTests.cs
+++ libs/Outbox/Shared.Outbox.Tests/Extensions/OutboxExtensionsTests.cs
@@ -31,7 +31,7 @@ public class OutboxExtensionsTests
         }
     }
 
-    private static IServiceCollection CreateBaseServices()
+    private static ServiceCollection CreateBaseServices()
     {
         var services = new ServiceCollection();
         services.AddLogging();
